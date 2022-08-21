8f0aa6ccc0a48c7ee0aca8d9a2111026d0cea340
Evans Ochieng <evansochieng121@gmail.com>
2022-08-21 11:01:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// Change the text content of p tag after the DOM is fully built
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.querySelector('#text');
+    p.textContent = "This is really cool!";
+})
+
+// Alternative to 'DOMContentLoaded' is 'defer' attribute in <script> tages
+// <script src="index.js" defer></script>
